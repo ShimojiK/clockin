21ea0f38b1f1d8f6d70b9b27160a60d0c81f967b
techno-tanoC <develop.tanoc@gmail.com>
2015-04-27 18:58:28 +0900

    user can record the time

--- app/controllers/user/time_logs_controller.rb
+++ app/controllers/user/time_logs_controller.rb
@@ -4,9 +4,18 @@ class User::TimeLogsController < User::Base
   end
 
   def new
+    @time_log = current_user.time_logs.find_or_initialize_by(end_at: nil)
   end
 
   def create
+    time_log = current_user.time_logs.find_or_initialize_by(end_at: nil)
+    if time_log.start_at
+      time_log.update(end_at: Time.now)
+    else
+      time_log.start_at = Time.now
+      time_log.save
+    end
+    redirect_to :back
   end
 
   def update
