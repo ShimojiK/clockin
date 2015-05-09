280445f2ca89a2689721e671dcf238ebe4579b58
techno-tanoC <develop.tanoc@gmail.com>
2015-05-09 21:31:42 +0900

    impl admin time_log

--- app/controllers/admin/comments_controller.rb
+++ app/controllers/admin/comments_controller.rb
@@ -1,6 +1,7 @@
 class Admin::CommentsController < Admin::Base
   def index
-    @comments = TimeLog.find(params[:time_log_id]).comments
+    @time_log = TimeLog.find(params[:time_log_id])
+    @comments = @time_log.comments
   end
 
   def create
--- app/controllers/admin/time_logs_controller.rb
+++ app/controllers/admin/time_logs_controller.rb
@@ -5,5 +5,21 @@ class Admin::TimeLogsController < Admin::Base
   end
 
   def update
+    time_log = TimeLog.find(params[:id])
+    old_end = time_log.end_at
+    if time_log.update(time_log_param)
+      if time_log.create_update_comment(old_end)
+        redirect_to :back
+      else
+        redirect_to :back, notice: "コメントの投稿に失敗しました(時刻の変更には問題ありません"
+      end
+    else
+      redirect_to :back, alert: "更新に失敗しました"
+    end
+  end
+
+  private
+  def time_log_param
+    params.require(:time_log).permit(:start_at, :end_at)
   end
 end
