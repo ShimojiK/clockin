8f65a48b185dce60d11fe95029ea31c3ffa932a5
techno-tanoC <develop.tanoc@gmail.com>
2015-05-21 23:03:48 +0900

    fix association

--- app/models/user_comment.rb
+++ app/models/user_comment.rb
@@ -1,6 +1,6 @@
 class UserComment < Comment
   has_one :user, through: :time_log
-  belongs_to :ack_admin, class: :admin
+  belongs_to :ack_admin, class_name: "Admin"
 
   validates_presence_of :status
 end
