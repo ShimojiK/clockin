42a9f0bcf11d43e34fa92cb04b4139d41949ebf6
techno-tanoC <develop.tanoc@gmail.com>
2015-08-14 23:39:15 +0900

    change-password spec

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -3,11 +3,52 @@ require 'rails_helper'
 feature 'User management' do
   let(:user) { FactoryGirl.create :user }
 
-  scenario 'user log in' do
+  background do
     visit root_path
     fill_in 'アカウント名', with: user.account
     fill_in 'パスワード', with: "password"
     click_button 'ログイン'
+  end
+
+  scenario 'user log in' do
     expect(page).to have_content 'ログインしました'
+  end
+
+end
+
+feature "New user management" do
+  let(:admin) { FactoryGirl.create :admin }
+  let(:user) { FactoryGirl.build :user }
+
+  scenario "required to change password" do
+    # login admin
+    visit signin_admins_path
+    fill_in 'アカウント', with: admin.account
+    fill_in 'パスワード', with: "password"
+    click_button 'ログイン'
+
+    # create user
+    visit new_admin_user_path
+    fill_in '名前', with: user.name
+    fill_in '説明', with: user.desc
+    fill_in 'アカウント名', with: user.account
+    fill_in 'パスワード', with: "password"
+    click_button '保存'
+
+    # logout
+    visit admin_users_path
+    click_link 'signout'
+
+    # login user
+    visit root_path
+    fill_in 'アカウント名', with: user.account
+    fill_in 'パスワード', with: "password"
+    click_button 'ログイン'
+
+    # change password
+    fill_in '新しいパスワード', with: "new-password"
+    click_button '更新'
+
+    expect(page).to have_content 'パスワードを更新しました'
   end
 end
