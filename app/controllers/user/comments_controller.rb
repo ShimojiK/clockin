10b8fc5a7e6fade7051217ca2b84dbbf524367c0
techno-tanoC <develop.tanoc@gmail.com>
2015-10-31 14:59:45 +0900

    fix post comment redirect

--- app/controllers/user/comments_controller.rb
+++ app/controllers/user/comments_controller.rb
@@ -1,7 +1,7 @@
 class User::CommentsController < User::Base
   def create
     comment = current_user.time_logs.find(params[:time_log_id]).user_comments.new(comment_params)
-    redirect_to time_log_comments_path(params[:time_log_id]), alert: (comment.save ? nil : "コメントの投稿に失敗しました")
+    redirect_to time_log_path(params[:time_log_id]), alert: (comment.save ? nil : "コメントの投稿に失敗しました")
   end
 
   private
