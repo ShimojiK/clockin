70b22a3e11bcb6fc5f3093df0daa3973fbd84f59
techno-tanoC <develop.tanoc@gmail.com>
2015-11-07 18:52:36 +0900

    implement newest and oldest year

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,23 @@ class User < ActiveRecord::Base
   has_many :time_logs
   has_many :comments, through: :time_logs
   has_many :user_comments, through: :time_logs
+
+  def newest_year
+    time_logs.maximum(:original_start_at).year
+  end
+
+  def oldest_year
+    time_logs.minimum(:original_start_at).year
+  end
+
+  # get both year by once
+  def newest_and_oldest_year
+    hash = ActiveRecord::Base.connection.execute(<<-SQL).first
+      SELECT MAX(original_start_at) AS max, MIN(original_start_at) AS min FROM time_logs WHERE user_id = #{self.id};
+    SQL
+    {
+      max: hash["max"].to_date.year,
+      min: hash["min"].to_date.year
+    }
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,4 +8,29 @@ RSpec.describe User, type: :model do
     it { is_expected.to have_many(:comments).through(:time_logs) }
     it { is_expected.to have_many(:user_comments).through(:time_logs) }
   end
+
+  let(:user) { FactoryGirl.create :user }
+  let!(:old_time_log) { FactoryGirl.create :time_log, user: user, original_start_at: Time.utc(2013) }
+  let!(:new_time_log) { FactoryGirl.create :time_log, user: user }
+  describe "#newest_year" do
+    subject { user.newest_year }
+    it "is newest year" do
+      expect(subject).to be 2015
+    end
+  end
+
+  describe "#oldest_year" do
+    subject { user.oldest_year }
+    it "is oldest year" do
+      expect(subject).to be 2013
+    end
+  end
+
+  describe "#newest_and_oldest_year" do
+    it "returns both newest and oldest year" do
+      hash = user.newest_and_oldest_year
+      expect(hash[:max]).to be 2015
+      expect(hash[:min]).to be 2013
+    end
+  end
 end
