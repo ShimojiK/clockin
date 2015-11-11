9fc80c83bb2856724e41f8c6dfa36c3e0adfc33f
techno-tanoC <develop.tanoc@gmail.com>
2015-11-11 20:44:31 +0900

    fix bug when user has no time_log

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,11 +6,11 @@ class User < ActiveRecord::Base
   has_many :user_comments, through: :time_logs
 
   def newest_year
-    time_logs.maximum(:original_start_at).year
+    time_logs.maximum(:original_start_at).try(&:year) || Time.zone.now.year
   end
 
   def oldest_year
-    time_logs.minimum(:original_start_at).year
+    time_logs.minimum(:original_start_at).try(&:year) || Time.zone.now.year
   end
 
   # get both year by once
@@ -18,9 +18,11 @@ class User < ActiveRecord::Base
     hash = ActiveRecord::Base.connection.execute(<<-SQL).first
       SELECT MAX(original_start_at) AS max, MIN(original_start_at) AS min FROM time_logs WHERE user_id = #{self.id};
     SQL
-    {
-      max: hash["max"].to_date.year,
-      min: hash["min"].to_date.year
-    }
+    hash.try do
+      {
+        max: hash["max"].to_date.year,
+        min: hash["min"].to_date.year
+      }
+    end || Time.zone.now.year
   end
 end
