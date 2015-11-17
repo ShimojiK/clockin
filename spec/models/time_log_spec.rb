ad897dc5ea45a7f3e644f44663e25ba07dac2297
techno-tanoC <develop.tanoc@gmail.com>
2015-11-17 21:27:25 +0900

    refactor TimeLog spec

--- spec/models/time_log_spec.rb
+++ spec/models/time_log_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe TimeLog, type: :model do
+  let(:user) { FactoryGirl.create :user }
+
   describe "associations" do
     it { is_expected.to belong_to :user }
     it { is_expected.to have_many :comments }
@@ -13,59 +15,90 @@ RSpec.describe TimeLog, type: :model do
     it { is_expected.not_to be_valid }
   end
 
-  describe "#lengthen?" do
-    let(:param) { params_from_time(Time.now + 10.minute, :end_at) }
+  describe "#updatable_check" do
+    let(:user) { FactoryGirl.create :user }
+    let(:time_log) { FactoryGirl.create :time_log, user: user }
+    subject { time_log.errors.count }
 
-    it "is true " do
-      time_log = FactoryGirl.build :time_log
-      expect(time_log.send(:lengthen?, param)).to be true
+    context "when valid" do
+      it { is_expected.to be_zero }
+    end
+
+    context "when invalid" do
+      context "lengthen" do
+        before do
+          time_log.updatable_check(params_from_time(Time.now + 1.hour, :end_at))
+        end
+        it { is_expected.not_to be_zero }
+      end
+
+      context "unfinished" do
+        before do
+          time_log.original_end_at = nil
+          time_log.updatable_check
+        end
+        it { is_expected.not_to be_zero }
+      end
+
+      context "non last" do
+        before do
+          time_log
+          FactoryGirl.create :time_log, user: user
+          time_log.updatable_check
+        end
+        it { is_expected.not_to be_zero }
+      end
+
+      context "time up" do
+        before do
+          time_log.original_end_at = Time.now - 2.hour
+          time_log.updatable_check
+        end
+        it { is_expected.not_to be_zero }
+      end
     end
   end
 
   describe "#user_updatable?" do
-    it "is true" do
-      time_log = FactoryGirl.create :time_log_with_user
-      expect(time_log.user_updatable?).to be true
+    let(:time_log) { FactoryGirl.create :time_log_with_user }
+    subject { time_log.user_updatable? }
+
+    context "when updatable" do
+      it { is_expected.to be_truthy }
     end
 
-    it "is falsy on passing 1 hour and over" do
-      time_log = FactoryGirl.create :time_log_with_user, original_end_at: Time.now - 2.hour
-      expect(time_log.user_updatable?).to be_falsy
+    context "when passing 1 hour" do
+      before { time_log.original_end_at = Time.now - 2.hour }
+      it { is_expected.to be_falsy }
     end
 
-    it "is falsy on non last time_log" do
-      user = FactoryGirl.create :user
-      time_logs = FactoryGirl.create_list :time_log, 3, user: user
-      expect(time_logs.last.user_updatable?).to be true
-      expect(time_logs.first.user_updatable?).to be_falsy
+    context "when non last time_log" do
+      let(:time_logs) { FactoryGirl.create_list :time_log, 3, user: user }
+      subject { time_logs.first.user_updatable? }
+      it { is_expected.to be_falsy }
     end
 
-    it "is falsy on unfinished time_log" do
-      time_log = FactoryGirl.create :time_log_with_user, original_end_at: nil
-      expect(time_log.user_updatable?).to be_falsy
+    context "when unfinished time_log" do
+      before { time_log.original_end_at = nil }
+      it { is_expected.to be_falsy }
     end
   end
 
   describe "#update_with_create_user_comment" do
-    it "creates user comment on success" do
-      time_log = FactoryGirl.create :time_log_with_user
-      end_at = time_log.end_at - 1.minute
-      time = params_from_time(time_log.end_at - 1.minute, :end_at)
-      expect {
-        time_log.update_with_create_user_comment(time)
-      }.to change{ UserComment.count }.from(0).to(1)
-    end
+    let(:time_log) { FactoryGirl.create :time_log_with_user }
+    let(:end_at) { time_log.end_at - 1.minute }
+    let(:time) { params_from_time(time_log.end_at - 1.minute, :end_at) }
+    subject { time_log.update_with_create_user_comment(time) }
+
+    it { lead_to change{ UserComment.count }.from(0).to(1) }
   end
 
   describe "#update_with_create_admin_comment" do
-    it "return { alert: nil } on success" do
-      admin = FactoryGirl.create :admin
-      time_log = FactoryGirl.create :time_log
-      end_at = time_log.end_at - 1.minute
-      time = params_from_time(time_log.end_at - 1.minute, :end_at)
-      expect {
-        time_log.update_with_create_admin_comment(time, admin)
-      }.to change{ AdminComment.count }.from(0).to(1)
-    end
+    let(:admin) { FactoryGirl.create :admin }
+    let(:time_log) { FactoryGirl.create :time_log }
+    let(:time) { params_from_time(time_log.end_at - 1.minute, :end_at) }
+    subject { time_log.update_with_create_admin_comment(time, admin) }
+
+    it { lead_to change{ AdminComment.count }.from(0).to(1) }
   end
 end
