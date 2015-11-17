54ac15391dd5f7a2dd74516d339eb99314fe2ef6
techno-tanoC <develop.tanoc@gmail.com>
2015-11-17 18:08:17 +0900

    divide feature spec file

---
+++ spec/features/user/time_logs/clock_in_and_out_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+feature "time_logs" do
+  let(:user) { FactoryGirl.create :user }
+
+  background do
+    user_login
+  end
+
+  scenario "clock in" do
+    visit time_logs_path
+    expect {
+      click_button 'Start'
+    }.to change{ user.time_logs.count }.from(0).to(1)
+    expect(user.time_logs.first.start_at).not_to be_nil
+    expect(user.time_logs.first.end_at).to be_nil
+  end
+
+  scenario "clock out" do
+    visit time_logs_path
+    click_button 'Start'
+    click_button 'Stop'
+    expect(user.time_logs.first.end_at).not_to be_nil
+  end
+end
