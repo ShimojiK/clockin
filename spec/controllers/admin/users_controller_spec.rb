3707d838390236d2a7602b69d034c8d5753a97d5
techno-tanoC <develop.tanoc@gmail.com>
2015-11-21 21:47:35 +0900

    refactor admin users controller spec

--- spec/controllers/admin/users_controller_spec.rb
+++ spec/controllers/admin/users_controller_spec.rb
@@ -9,68 +9,64 @@ RSpec.describe Admin::UsersController, type: :controller do
   end
 
   describe "GET index" do
+    before { get :index }
+
     it "assigns users" do
-      get :index
       expect(assigns(:users)).to eq [user]
     end
 
-    it "renders index" do
-      get :index
-      expect(response).to render_template :index
-    end
+    it { expect(response).to render_template :index }
   end
 
   describe "GET new" do
+    before { get :new }
+
     it "assigns user" do
-      get :new
       expect(assigns(:user)).to be_a_new User
     end
 
-    it "renders new" do
-      get :new
-      expect(response).to render_template :new
-    end
+    it { expect(response).to render_template :new }
   end
 
   describe "POST create" do
+    subject { post :create, user: { name: "yazawa nico", desc: "25252", account: "nico", password: "password" } }
+
     it "create new user" do
-      expect {
-        post :create, user: { name: "yazawa nico", desc: "25252", account: "nico", password: "password" }
-      }.to change{ User.count }.from(0).to(1)
-      expect(User.last.name).to eq "yazawa nico"
-      expect(User.last.desc).to eq "25252"
+      leads.to change{ User.count }.from(0).to(1)
     end
 
-    it "redirects to admin_users_path" do
-      post :create, user: { name: "yazawa nico", desc: "25252", account: "nico", password: "password" }
-      expect(response).to redirect_to admin_users_path
+    it "has name" do
+      leads{ User.last.name }.to eq "yazawa nico"
     end
+
+    it "has desc" do
+      leads{ User.last.desc }.to eq "25252"
+    end
+
+    it { leads{ response }.to redirect_to admin_users_path }
   end
 
   describe "GET edit" do
+    subject { get :edit, id: user.id }
+
     it "assigns user" do
-      get :edit, id: user.id
-      expect(assigns(:user)).to eq user
+      leads{ assigns(:user) }.to eq user
     end
 
-    it "renders edit" do
-      get :edit, id: user.id
-      expect(response).to render_template :edit
-    end
+    it { leads{ response }.to render_template :edit }
   end
 
   describe "PATCH update" do
-    it "update user" do
-      name = user.name
-      expect {
+    subject do
         patch :update, id: user.id, user: { name: "ayase eli", desc: "harasyo", account: "eli" }
         user.reload
-      }.to change{ user.name }.from(name).to("ayase eli").and change{ user.desc }.to("harasyo").and change{ user.account }.to("eli")
     end
 
-    it "redirects to admin_user_time_logs_path" do
-      patch :update, id: user.id, user: { name: "ayase eli", desc: "harasyo", account: "eli" }
-      expect(response).to redirect_to admin_user_time_logs_path(user)
+    it "update user" do
+      name = user.name
+      leads.to change{ user.name }.from(name).to("ayase eli").and change{ user.desc }.to("harasyo").and change{ user.account }.to("eli")
     end
+
+    it { leads{ response }.to redirect_to admin_user_time_logs_path(user) }
   end
 end
