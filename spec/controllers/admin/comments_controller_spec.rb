c5d6d031eded3dc42be11df4c6527c67fbc968e5
techno-tanoC <develop.tanoc@gmail.com>
2015-11-21 21:33:07 +0900

    add admin comments controller spec

--- spec/controllers/admin/comments_controller_spec.rb
+++ spec/controllers/admin/comments_controller_spec.rb
@@ -3,21 +3,33 @@ require 'rails_helper'
 RSpec.describe Admin::CommentsController, type: :controller do
   let(:admin) { FactoryGirl.create :admin }
   let(:user) { FactoryGirl.create :user }
-  let(:time_log) { FactoryGirl.create :time_log, user: user }
+  let!(:time_log) { FactoryGirl.create :time_log, user: user }
 
   before do
     session[:admin_id] = admin.id
   end
 
   describe "POST create" do
-    it "creates new admin comment" do
-      expect {
-        post :create, time_log_id: time_log.id, admin_comment: { body: "admin body" }
-      }.to change{ time_log.comments.count }.from(0).to(1)
-      expect(time_log.comments.first.body).to eq "admin body"
+    subject { post :create, time_log_id: time_log.id, admin_comment: { body: "admin body" } }
+
+    it "create comment body" do
+      leads{ time_log.comments.first.body }.to eq "admin body"
     end
   end
 
   describe "PUT update" do
+    let(:user_comment) { FactoryGirl.create :user_comment, time_log: time_log }
+    subject do
+      patch :update, id: user_comment.id, admin_comment: { status: 1 }
+      user_comment.reload
+    end
+
+    it "updates status" do
+      leads.to change{ user_comment.status }.from(0).to(1)
+    end
+
+    it "updates ack_admin_id" do
+      leads{ user_comment.ack_admin_id }.to eq admin.id
+    end
   end
 end
